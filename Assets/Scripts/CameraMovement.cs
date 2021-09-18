cbd8180cd46070708195032d4f3efc00081b5a7d
Roman Anoshin <anoshin97@gmail.com>
2021-09-18 13:15:00 +0300

    Transfer files to the folder up.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 DistanceToTarget;
+
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        if (IsOnPosition()) {
+            transform.position = new Vector3(transform.position.x - DistanceToTarget.x, transform.position.y - DistanceToTarget.y, Target.position.z - DistanceToTarget.z);
+        }
+    }
+
+    private bool IsOnPosition()
+    {
+        return Target.position + DistanceToTarget != transform.position;
+    }
+}
