6fe803c5758fddad3cddffb55c3be24584a9f987
Roman Anoshin <anoshin97@gmail.com>
2021-11-16 18:39:28 +0300

    Refactoring Road system. Add road generation through сompatible blocks. Add optimization.

--- Assets/Scripts/Road/GenerationMap.cs
+++ Assets/Scripts/Road/GenerationMap.cs
@@ -6,108 +6,69 @@ namespace Road
 {
     public class GenerationMap
     {
-        private GameObject[][] PrefabsWithSegments;
-        private int[] UniqueDifficults;
-        private int[][] RepetitionCountsMap;
+        public GameObject[][] SegmentedPrefabs { get; private set; }
+        public RoadBlockData[][] BlocksData { get; private set; }
+        public int[][] RepetitionCounts { get; private set; }
+        public int[] UniqueDifficults { get; private set; }
 
         public GenerationMap(List<GameObject> sortedRoadBlocks)
         {
             PrepareSegmentsMap(sortedRoadBlocks);
+            PrepareUniqueDifficults();
+            PrepareRepetitionCounts();
         }
 
         private void PrepareSegmentsMap(List<GameObject> sortedRoadBlocks)
         {
-            List<int> uniqueDifficults = new List<int>();
             List<int> uniqueItemIndices = new List<int>();
+            var notSegmentedBlocksData = new RoadBlockData[sortedRoadBlocks.Count];
 
-            uniqueDifficults.Add(sortedRoadBlocks[0].GetComponent<RoadBlock>().Difficult);
+            notSegmentedBlocksData[0] = sortedRoadBlocks[0].GetComponent<RoadBlockData>();
+            var previousDifficult = notSegmentedBlocksData[0].Difficult;
             uniqueItemIndices.Add(0);
 
             for (var i = 1; i < sortedRoadBlocks.Count; i++) {
-                var difficult = sortedRoadBlocks[i].GetComponent<RoadBlock>().Difficult;
-                if (uniqueDifficults[uniqueDifficults.Count - 1] != difficult) {
-                    uniqueDifficults.Add(difficult);
+                notSegmentedBlocksData[i] = sortedRoadBlocks[i].GetComponent<RoadBlockData>();
+                var difficult = notSegmentedBlocksData[i].Difficult;
+                if (previousDifficult != difficult) {
+                    previousDifficult = difficult;
                     uniqueItemIndices.Add(i);
                 }
             }
-            uniqueItemIndices.Add(sortedRoadBlocks.Count); // add end
+            uniqueItemIndices.Add(sortedRoadBlocks.Count);
 
-            UniqueDifficults = new int[uniqueDifficults.Count];
-            for (var i = 0; i < uniqueDifficults.Count; i++) {
-                UniqueDifficults[i] = uniqueDifficults[i];
-            }
-
-            PrefabsWithSegments = new GameObject[uniqueDifficults.Count][];
-            for (var i = 0; i < uniqueDifficults.Count; i++) {
+            SegmentedPrefabs = new GameObject[uniqueItemIndices.Count - 1][];
+            BlocksData = new RoadBlockData[SegmentedPrefabs.Length][];
+            for (var i = 0; i < uniqueItemIndices.Count - 1; i++) {
                 var dimension = uniqueItemIndices[i + 1] - uniqueItemIndices[i];
-                PrefabsWithSegments[i] = new GameObject[dimension];
+                SegmentedPrefabs[i] = new GameObject[dimension];
+                BlocksData[i] = new RoadBlockData[SegmentedPrefabs[i].Length];
                 for (var j = 0; j < dimension; j++) {
-                    PrefabsWithSegments[i][j] = sortedRoadBlocks[uniqueItemIndices[i] + j];
+                    SegmentedPrefabs[i][j] = sortedRoadBlocks[uniqueItemIndices[i] + j];
+                    BlocksData[i][j] = notSegmentedBlocksData[uniqueItemIndices[i] + j];
                 }
-            }
-
-            RepetitionCountsMap = new int[PrefabsWithSegments.Length][];
-            for (var i = 0; i < RepetitionCountsMap.Length; i++) {
-                RepetitionCountsMap[i] = new int[PrefabsWithSegments[i].Length];
             }
         }
 
-        public GameObject GetBlockByDifficulty(float difficult)
+        private void PrepareRepetitionCounts()
         {
-            var indexInFirstRow = 0;
-            if (UniqueDifficults.Length > 1) {
-                var upperIndex = GetUpperBound(UniqueDifficults, difficult);
-                var lowerIndex = upperIndex - 1;
-                if (
-                    lowerIndex > 0 &&
-                    difficult - UniqueDifficults[lowerIndex] < UniqueDifficults[upperIndex] - difficult
-                ) {
-                    indexInFirstRow = lowerIndex;
-                } else {
-                    indexInFirstRow = upperIndex;
-                }
-            }
-            return GetBlockWithFewerRepetitions(indexInFirstRow);
-        }
-
-        private GameObject GetBlockWithFewerRepetitions(int indexInFirstRow)
-        {
-            if (RepetitionCountsMap[indexInFirstRow].Length > 1) {
-                var minValue = RepetitionCountsMap[indexInFirstRow][0];
-                var indexOfMin = 0;
-                for (var i = 1; i < RepetitionCountsMap[indexInFirstRow].Length; i++) {
-                    if (RepetitionCountsMap[indexInFirstRow][i] < minValue) {
-                        minValue = RepetitionCountsMap[indexInFirstRow][i];
-                        indexOfMin = i;
-                    }
-                }
-
-                RepetitionCountsMap[indexInFirstRow][indexOfMin]++;
-                return PrefabsWithSegments[indexInFirstRow][indexOfMin];
-            } else {
-                RepetitionCountsMap[indexInFirstRow][0]++;
-                return PrefabsWithSegments[indexInFirstRow][0];
+            RepetitionCounts = new int[SegmentedPrefabs.Length][];
+            for (var i = 0; i < RepetitionCounts.Length; i++) {
+                RepetitionCounts[i] = new int[SegmentedPrefabs[i].Length];
             }
         }
 
-        private int GetUpperBound(int[] array, float value)
+        private void PrepareUniqueDifficults()
         {
-            var first = 0;
-            var count = array.Length - 1 - first;
-            int step;
-            int i;
-            while(count > 0) {
-                i = first;
-                step = count / 2;
-                i += step;
-                if(!(value < array[i])) {
-                    first = ++i;
-                    count -= step + 1;
-                } else {
-                    count = step;
-                }
+            UniqueDifficults = new int[BlocksData.Length];
+            for (var i = 0; i < UniqueDifficults.Length; i++) {
+                UniqueDifficults[i] = BlocksData[i][0].Difficult;
             }
-            return first;
+        }
+
+        public void UpRepetitionCount(int segmentIndex, int elementIndex)
+        {
+            RepetitionCounts[segmentIndex][elementIndex]++;
         }
     }
 }
---
+++ Assets/Scripts/Utilities/MyMath.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MyMath
+{
+    public static int GetNearestIndex(int[] collection, float value)
+    {
+        if (collection.Length > 1) {
+            var upperIndex = GetUpperBound(collection, value);
+            var lowerIndex = upperIndex - 1;
+            if (
+                lowerIndex > 0 &&
+                value - collection[lowerIndex] < collection[upperIndex] - value
+            ) {
+                return lowerIndex;
+            } else {
+                return upperIndex;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    public static int GetUpperBound(int[] array, float value)
+    {
+        var first = 0;
+        var count = array.Length - 1 - first;
+        int step;
+        int i;
+        while (count > 0) {
+            i = first;
+            step = count / 2;
+            i += step;
+            if (!(value < array[i])) {
+                first = ++i;
+                count -= step + 1;
+            } else {
+                count = step;
+            }
+        }
+        return first;
+    }
+
+    public static int GetIndexOfMinByIndexes(int[] collection, int[] indexes)
+    {
+        if (collection.Length > 1) {
+            var minValue = collection[indexes[0]];
+            var indexOfMin = indexes[0];
+            foreach(var i in indexes) {
+                if (collection[i] < minValue) {
+                    minValue = collection[i];
+                    indexOfMin = i;
+                }
+            }
+            return indexOfMin;
+        } else {
+            return 0;
+        }
+    }
+
+    public static bool IsContains(int[] collection, int value)
+    {
+        for (var i = 0; i < collection.Length; i++) {
+            if (collection[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
