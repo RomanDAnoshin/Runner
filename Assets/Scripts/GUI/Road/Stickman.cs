1134ac9d3f546667de8a3116d391134aeb3978ca
Roman Anoshin <anoshin97@gmail.com>
2022-02-13 18:30:56 +0300

    Add Stickman animation in Road GUI.

---
+++ Assets/Scripts/GUI/Road/Stickman.cs
@@ -0,0 +1,44 @@
+﻿using Game;
+using Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stickman : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+
+        PlayerInput.Instance.Ran += OnPlayerRan;
+        GameData.Instance.Lost += OnGameLost;
+        PlayerData.Instance.CurrentSpeedModificatorChanged += OnSpeedModificatorChanged;
+        animator.enabled = false;
+    }
+
+    private void OnPlayerRan()
+    {
+        PlayerInput.Instance.Ran -= OnPlayerRan;
+        animator.enabled = true;
+    }
+
+    private void OnGameLost()
+    {
+        GameData.Instance.Lost -= OnGameLost;
+        animator.enabled = false;
+    }
+
+    private void OnSpeedModificatorChanged()
+    {
+        animator.speed = PlayerData.Instance.CurrentSpeedModificator;
+    }
+
+    void OnDestroy()
+    {
+        PlayerData.Instance.CurrentSpeedModificatorChanged -= OnSpeedModificatorChanged;
+        PlayerInput.Instance.Ran -= OnPlayerRan;
+        GameData.Instance.Lost -= OnGameLost;
+    }
+}
